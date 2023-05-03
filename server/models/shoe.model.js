f83768a3d5b070b0018ce7825acf24b0329ccf81
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-05-03 02:05:19 -0600

    shoe model

---
+++ server/models/shoe.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const GigSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    totalHype: {
+      type: Number,
+      default: 0,
+    },
+    hypeNumber: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    cover: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: [String],
+      required: false,
+    },
+    deliveryTime: {
+      type: Number,
+      required: true,
+    },
+    sales: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Gig", GigSchema);

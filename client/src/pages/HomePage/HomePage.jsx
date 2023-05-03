080a97c3b9b01e6c0e0713a5dc25655629ecaacc
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-05-03 04:55:56 -0600

    ss

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
       <div className="hero-section">
         <h1 className="hero-title">Welcome to Fyre</h1>
         <p className="hero-text">Find the perfect pair of shoes for every occasion.</p>
-        <button className="shop-now-button">Shop Now</button>
+      <a href="/shoes">  <button className="shop-now-button">Shop Now</button></a>
       </div>
     </div>
   );

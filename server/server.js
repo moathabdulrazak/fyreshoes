18a8b2e7af3f6b0f2c6eb8d303bbae5ecd265f6a
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-05-03 01:57:39 -0600

    cookie parser imported

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from 'dotenv'
 import mongoose from "mongoose";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express()
 dotenv.config()
@@ -17,7 +18,8 @@ const connect = async () => {
 };
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-
+app.use(express.json())
+app.use(cookieParser())
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
   const errorMessage = err.message || "Something went wrong!";

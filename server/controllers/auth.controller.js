aba5590afb77b1b3d091ce15162593004bc3803e
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-05-03 02:08:35 -0600

    auth route and controller

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,27 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import createError from "../utils/createError.js";
+
+export const register = async (req, res, next) => {
+try {
+  
+} catch (error) {
+  next(error)
+}
+};
+
+export const login = async (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    next(error)
+  }
+};
+export const logout =  async (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    next(error)
+  }
+};

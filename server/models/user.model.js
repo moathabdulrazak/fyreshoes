dd4c91b30efff5b8e0d0fac25300f8d3ab4db22d
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-05-03 02:03:23 -0600

    user model

---
+++ server/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  img: {
+    type: String,
+    required: false,
+  },
+  desc: {
+    type: String,
+    required: false,
+  },
+},{
+  timestamps:true
+});
+
+export default mongoose.model("User", userSchema)

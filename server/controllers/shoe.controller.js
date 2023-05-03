66be918ddb89f6e75ec4752de08eae1abb70ad41
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-05-03 02:41:18 -0600

    create shoe BE

--- server/controllers/shoe.controller.js
+++ server/controllers/shoe.controller.js
@@ -2,8 +2,13 @@ import shoe from '../models/shoe.model.js'
 import createError from "../utils/createError.js"
 
 export const createShoe = async (req, res, next) => {
+  const newShoe = new shoe({
+    userId: req.userId,
+    ...req.body
+  })
   try {
-      
+    const savedShoe = await newShoe.save()
+    res.status(201).json(savedShoe)
   } catch (error) {
     next(error)
   }
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,18 +2,18 @@ import User from "../models/user.model.js";
 import createError from "../utils/createError.js";
 
 export const deleteUser = async (req, res, next) => {
-   try {
-    
-   } catch (error) {
-    next(error)
-   }
+  const user = await User.findById(req.params.id);
+
+
+    if (req.userId !== user?._id.toString()) {
+      return next(createError(403, "You can only delete your account"));
+    }
+    await User.findByIdAndDelete(req.params.id)
+    res.status(200).send('deleted')
   
 };
 export const getUser = async (req, res, next) => {
-  try {
-    
-  } catch (error) {
-   next(error)
-  }
- 
+  const user = await User.findById(req.params.id);
+
+  res.status(200).send(user);
 };

acff4f54a50dd882b840a2a58cdb2adf192ebf82
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-05-03 02:32:35 -0600

    logging in

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -20,7 +20,23 @@ export const register = async (req, res, next) => {
 
 export const login = async (req, res, next) => {
   try {
-    
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return next(createError(404, "User not found!"));
+
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    if (!isCorrect) return next(createError(404, "Wrong password or username"));
+
+    const token = jwt.sign({
+      id: user._id,
+    // @ts-ignore
+    }, process.env.JWT );
+
+    // @ts-ignore
+    const { password, ...info } = user._doc;
+
+    res.cookie("accessToken", token, {
+      httpOnly: "true"
+    }).status(200).send(info);
   } catch (error) {
     next(error)
   }

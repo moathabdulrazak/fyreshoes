f030140622914c2ebd70a6e318a689897d5112c0
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-05-03 01:55:16 -0600

    error handler

--- server/server.js
+++ server/server.js
@@ -14,6 +14,12 @@ const connect = async () => {
     console.log(error);
   }
 };
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+
+  return res.status(errorStatus).send(errorMessage);
+});
 
 app.listen(3000, () => {
   connect()
